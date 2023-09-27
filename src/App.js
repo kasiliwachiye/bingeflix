ee83617470b2745d7fbfef7bfe25228fa6b16e9c
FrankMutethia <frankmutethia81@gmail.com>
2023-09-27 23:04:08 +0300

    header and footer flowbite setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <AllRoutes/>
+      <Header />
+      <AllRoutes />
+      <Footer />
+     
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 //all of them are elements
-
-export {Header} from './Header';
-export {Footer} from './Footer';
+export { Header } from "./Header";
+export { Footer } from "./Footer";
 export {Card} from './Card';
