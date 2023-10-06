e7cb8fab019fac902b42acbd58db590006fcdb99
FrankMutethia <frankmutethia81@gmail.com>
2023-10-06 13:42:22 +0300

    dark mode intergration with tailwind css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
 // import { Header } from './Header';
@@ -6,7 +6,21 @@ import Logo from "../assets/logo.png";
 
 export const Header = () => {
 
- const [hidden, setHidden] = useState(false); 
+ const [hidden, setHidden] = useState(true); 
+ const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") ||true);
+
+ useEffect (() => {
+  localStorage.setItem("darkMode", JSON.stringify(darkMode));
+
+  if(darkMode){
+    document.documentElement.classList.add('dark');
+  }
+  else
+  {
+   document.documentElement.classList.remove('dark');
+  }
+
+ }, [darkMode]);
   
 //   const activeclassName = "text-base block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
 //   const inActiveclassName = "text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
@@ -15,8 +29,7 @@ const activeclassName =  "text-base block py-2 pr-4 pl-3 text-white bg-blue-700 
 const inActiveclassName = "text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
   return (
     <header>
-
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <Link to="/" className="flex items-center">
               <img src={Logo} className="mr-2 h-8 sm:h-9" alt="Cinemate Logo" />
@@ -26,7 +39,12 @@ const inActiveclassName = "text-base block py-2 pr-4 pl-3 text-gray-700 rounded 
           <img src= {Logo} classNameName="h-8 mr-3" alt="Cinemate Logo" />
           <span classNameName="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
       </Link> */}
-    <div className="flex md:order-2">
+    <div id= "mobile-nav" className="flex md:order-2">
+
+    <button onClick={() => setDarkMode(!darkMode)} data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip" type="button" data-toggle-dark="light" className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+            { darkMode ? (<svg aria-hidden="true" data-toggle-icon="sun" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path></svg>) : (<svg aria-hidden="true" data-toggle-icon="moon" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>) }
+          </button>
+
       <button  onClick= { () => setHidden(!hidden) } type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -50,7 +68,7 @@ const inActiveclassName = "text-base block py-2 pr-4 pl-3 text-gray-700 rounded 
           </svg>
       </button>
     </div>
-      <div className= {`${hidden ? "hidden" : ""} justify-between items-center hidden w-full md:flex md:w-auto md:order-1" id="navbar-search`}>
+      <div id="nav-links" className= {`${hidden ? "hidden" : ""} justify-between items-center hidden w-full md:flex md:w-auto md:order-1" id="navbar-search`}>
         <div className="relative mt-3 md:hidden">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
