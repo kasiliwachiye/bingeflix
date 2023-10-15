1a09fdc375587090b36c7e17ab716776bd7a5a69
FrankMutethia <frankmutethia81@gmail.com>
2023-10-15 23:34:38 +0300

    env API key setup

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,8 +4,7 @@ import { useState, useEffect } from "react";
 
 export const useFetch = (apiPath) => {
     const [data, setData] = useState([]);
-   const url = `https://api.themoviedb.org/3/${apiPath}?api_key=b80d59c33d6d57ed9c7e3713f91c188a
-   `;
+   const url = `https://api.themoviedb.org/3/${apiPath}?api_key=9c876a4f0e3552f73edb96a3c16b8aee`;
     // while fetching the movies data do not forget the url
 useEffect (() => { async function fetchMovies(){ 
     const response = await fetch(url);
