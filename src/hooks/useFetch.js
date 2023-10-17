b72234f4340bfa85c8e277d6d5b66c86a091906b
FrankMutethia <frankmutethia81@gmail.com>
2023-10-17 22:33:44 +0300

    API fetch movie search

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 
 
 
-export const useFetch = (apiPath) => {
+export const useFetch = (apiPath, queryTerm="") => {
     const [data, setData] = useState([]);
-   const url = `https://api.themoviedb.org/3/${apiPath}?api_key=9c876a4f0e3552f73edb96a3c16b8aee`;
+   const url = `https://api.themoviedb.org/3/${apiPath}?api_key=9c876a4f0e3552f73edb96a3c16b8aee&query=${queryTerm}`;
     // while fetching the movies data do not forget the url
 useEffect (() => { async function fetchMovies(){ 
     const response = await fetch(url);
