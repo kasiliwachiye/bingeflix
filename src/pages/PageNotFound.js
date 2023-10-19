78e4f19103dfc0ae357a444f07144b58fbaad358
FrankMutethia <frankmutethia81@gmail.com>
2023-10-19 23:07:19 +0300

    Page Not Found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,22 @@
-
+import PageNotFoundImage from "../assets/images/pagenotfound.png"
+import { Link } from "react-router-dom";
 export const PageNotFound = () => {
   return (
-   <main>PageNotFound</main>
+   <main>
+   <section className="flex flex-col justify-center px-2">
+    <div className="flex flex-col items-center my-4">
+ <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white">404, Oops!</p>
+<div className="max-w-lg">
+ <img className= "rounded"src={PageNotFoundImage} alt="404 Page Not Found" />
+    </div>
+    </div>
+    <div className="flex justify-center my-4">
+  <Link to="/"></Link>
+  {/* Adding gradient color */}
+  <button className="w-64 text-xl bg-gradient-to-r from-cyan-500 to-blue-500">Back To Cinemate</button>
+    </div>
+   </section>
+   </main>
   )
 }
 
