5aeb29204d8c52827e7b1a62796a603c2ea22786
FrankMutethia <frankmutethia81@gmail.com>
2023-10-22 10:37:28 +0300

    adding movie information: moviedetails component setup

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 
 export const useFetch = (apiPath, queryTerm="") => {
     const [data, setData] = useState([]);
-   const url = `https://api.themoviedb.org/3/${apiPath}?api_key=9c876a4f0e3552f73edb96a3c16b8aee&query=${queryTerm}`;
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=b80d59c33d6d57ed9c7e3713f91c188a&query=${queryTerm}`;
     // while fetching the movies data do not forget the url
 useEffect (() => { async function fetchMovies(){ 
     const response = await fetch(url);
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,8 +1,32 @@
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Backup from "../assets/images/backup.png";
 
 export const MovieDetail = () => {
+const params = useParams();
+// all the data will be stored here
+const [movie, setMovie] = useState({});
+const image = movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : Backup ;
+
+useEffect(() => {
+async function fetchMovie(){
+  const response = await fetch(`https://api.themoviedb.org/3/movie/{$param.id}?api_key=b80d59c33d6d57ed9c7e3713f91c188a&query`);
+  const json = await response.json()
+  setMovie(json);
+  console.log(json);
+}
+fetchMovie();
+}, [])
+
   return (
-  <main>MovieDetail</main>
+  <main>
+    <section className="flex justify-around flex-wrap py-5">
+      <div className="max-w-sm">
+      <img className="rounded" src={image} alt={movie.title} />
+      </div>
+      <div>{movie.title}</div>
+    </section>
+  </main>
   )
 }
 
